d1a0144064fbd1b47023bdac7c1a0825a0e9f2db
mymacbook <vokar@shernet.sheridancollege.ca>
2026-01-21 11:20:33 -0500

    Rotate script

---
+++ Assets/Script/RotateME.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
+
+public class RotateME : MonoBehaviour
+{
+    public float speed = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    { 
+    // Update is called once per frame
+    }
+    void Update()
+    {
+        Vector3 newRotation = transform.eulerAngles;
+        newRotation.z += speed * Time.deltaTime;
+        transform.eulerAngles = newRotation;
+    }
+}
