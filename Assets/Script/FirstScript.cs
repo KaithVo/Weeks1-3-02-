fc8941007850897f84bd737239a0d332bae81649
mymacbook <vokar@shernet.sheridancollege.ca>
2026-01-25 11:42:19 -0500

    keep it in the screen

--- Assets/Script/FirstScript.cs
+++ Assets/Script/FirstScript.cs
@@ -1,8 +1,9 @@
+
 using UnityEngine;
 
 public class FirstScript : MonoBehaviour
 {
-
+    float speed = 0.01f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,9 +12,18 @@ public class FirstScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //move the square
         Vector3 newPosition = transform.position;
-        newPosition.x += 0.1f;
+        newPosition.x += speed;
         transform.position = newPosition;
-    }
 
+        //check if the square is at the edge of the screen
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+        if (screenPos.x < 0 | screenPos.x > Screen.width)
+        {
+            //Y: multiply the speed by -1
+            speed = speed * -1;
+        }
+        
+    }
 }
