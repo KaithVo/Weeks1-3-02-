53c2784b5b754c78ca481e4f86922481ef132436
mymacbook <vokar@shernet.sheridancollege.ca>
2026-01-25 22:25:30 -0500

    Radom value

--- Assets/Script/FirstScript.cs
+++ Assets/Script/FirstScript.cs
@@ -1,14 +1,15 @@
-
 using UnityEngine;
 
 public class FirstScript : MonoBehaviour
 {
-    float speed = 0.01f;
+    public float speed = 0.01f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        //speed = Random. Range(0.01f, 0.30);
+        transform.position = (Vector2)transform.position + Random.insideUnitCircle * 2;
     }
+
     // Update is called once per frame
     void Update()
     {
@@ -19,11 +20,10 @@ public class FirstScript : MonoBehaviour
 
         //check if the square is at the edge of the screen
         Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
-        if (screenPos.x < 0 | screenPos.x > Screen.width)
+        if (screenPos.x < 0 || screenPos.x > Screen.width)
         {
             //Y: multiply the speed by -1
             speed = speed * -1;
         }
-        
     }
 }
