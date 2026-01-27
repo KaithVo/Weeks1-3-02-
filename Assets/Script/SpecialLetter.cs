b4183a64ae7e65e750cca89e7fcccc894c22cfb8
mymacbook <vokar@shernet.sheridancollege.ca>
2026-01-26 22:00:26 -0500

    Mouse recognized and color changed but not there yet, the letters are disapper when mouse over but the insperctor shows it is changing color ?

--- Assets/Script/SpecialLetter.cs
+++ Assets/Script/SpecialLetter.cs
@@ -5,10 +5,13 @@ using UnityEngine.InputSystem;
 public class SpecialLetter : MonoBehaviour
 {
     public bool mouseIsOverMe = false;
+    public Color col;
+    public SpriteRenderer spriteRenderer; //for sprite chnge
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -16,8 +19,10 @@ public class SpecialLetter : MonoBehaviour
     {
         //get the mouse position (in pixels) and convert it to world space (in meters)
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+
         //get the distance between transform.position and the mouse position
         float distance = Vector2.Distance(transform.position, mousePos);
+
         //if that distance is small (<1) then set mouseIsOverMe to true
         if (distance < 1)
         {
@@ -29,5 +34,21 @@ public class SpecialLetter : MonoBehaviour
             mouseIsOverMe = false;
 
         }
+        if (spriteRenderer.bounds.Contains(mousePos) == true)
+        {
+            //use the color variable
+            spriteRenderer.color = col;
+
+        }
+        else
+        {
+            //set the color to white
+            spriteRenderer.color = Color.white;
+        }
+
+    }
+    void Pickrandomcolor()
+    {
+        spriteRenderer.color = Random.ColorHSV();
     }
 }
