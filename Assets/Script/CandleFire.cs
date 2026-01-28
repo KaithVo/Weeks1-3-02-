61eb9c20f0252617de7fbfa1a96c48d1e7f78a2e
mymacbook <vokar@shernet.sheridancollege.ca>
2026-01-27 22:13:17 -0500

    if over 0.5f, change sprite1 to sprite 2 back and forth

--- Assets/Script/CandleFire.cs
+++ Assets/Script/CandleFire.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class CandleFire : MonoBehaviour
 {
+    //Calling candle light sprite
+    public Sprite light1;
+    public Sprite light2;
+
+    // calling spriteRender
+    public SpriteRenderer spriteRenderer;
+
+    //change sprite speed
+    public float t;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +21,16 @@ public class CandleFire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        t += Time.deltaTime;
+
+        //if over 0.5f, change sprite1 to sprite 2 back and forth
+        if (t >= 0.5f )
+        {
+            spriteRenderer.sprite = light1;
+        }
+        else
+        {
+            spriteRenderer.sprite = light2;
+        }
     }
 }
