82ccfe885ff13a08f7920b31087e461c91cca61f
mymacbook <vokar@shernet.sheridancollege.ca>
2026-01-27 21:39:17 -0500

    aghhhhhh it flikeringggg

--- Assets/Script/FlikeringLight.cs
+++ Assets/Script/FlikeringLight.cs
@@ -14,7 +14,7 @@ public class FlikeringLight : MonoBehaviour
     //animationCurve ofcours
     private float speed = 2f;
     public AnimationCurve flickeringCurve;
-    private float t; 
+    private float flickerTime; 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -29,13 +29,15 @@ public class FlikeringLight : MonoBehaviour
     }
     void Flickeringlight()// https://www.google.com/search?q=alpha+animation+curve+unity&sca_esv=dfd8ad97cdb7fcc2&rlz=1C5CHFA_enCA1026CA1026&sxsrf=ANbL-n4cs2UhHW8DzpZygbe8V6-kAJwHpw%3A1769565476840&ei=JG15aYyMM7Kew8cPgI3t8QQ&ved=0ahUKEwiMrp2Nka2SAxUyz_ACHYBGO04Q4dUDCBE&uact=5&oq=alpha+animation+curve+unity&gs_lp=Egxnd3Mtd2l6LXNlcnAiG2FscGhhIGFuaW1hdGlvbiBjdXJ2ZSB1bml0eTIFECEYoAEyBRAhGJ8FMgUQIRifBTIFECEYnwUyBRAhGJ8FMgUQIRifBTIFECEYnwUyBRAhGJ8FMgUQIRifBTIFECEYnwVIpBVQxgZYnBRwAXgBkAEAmAGgAaABsQWqAQMxLjW4AQPIAQD4AQGYAgegAs4FwgIKEAAYsAMY1gQYR8ICBxAhGKABGAqYAwCIBgGQBgiSBwMyLjWgB-IlsgcDMS41uAfMBcIHAzAuN8gHDoAIAA&sclient=gws-wiz-serp
     {
-        t += Time.deltaTime * speed;
+        //t is just a loop phase to go back and forth
+        //flickertime is the real goat, real clock
+        float t = flickerTime * 1f;
+        flickerTime += Time.deltaTime * speed;
 
-        float curveValue = flickeringCurve.Evaluate(t);
 
         //aplly alpha witout changing colpr
         Color currentColor = SpriteRenderer.color;
-        currentColor.a = Mathf.Lerp (minAlpha,maxAlpha,curveValue);
+        currentColor.a = Mathf.Lerp (minAlpha,maxAlpha, flickeringCurve.Evaluate(t));
         SpriteRenderer.color = currentColor;
     }
 }
